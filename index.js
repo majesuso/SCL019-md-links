f65b7ef7cc5f700f1e1031695eb6a84bc0cb7449
majesuso <majesus.op@gmail.com>
2022-04-07 13:16:49 -0400

    ingresar ruta

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
-};
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// readline.question('Ingrese la ruta del archivo: ', (path) => {
+//   console.log(`ruta: ${path}`);
+//   readline.close();
+// });
+
+const prompt = require ( 'prompt-sync' ) ( ) ; 
+let promptValue = prompt('Ingresa la ruta del archivo: ');
+console.log(`ruta: ${promptValue}`);
