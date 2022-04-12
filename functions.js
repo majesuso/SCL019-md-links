11e17f08e92af902fc0b21c9476ebe67e61a6973
majesuso <majesus.op@gmail.com>
2022-04-12 12:40:50 -0400

    leyendo archivo

---
+++ functions.js
@@ -0,0 +1,15 @@
+// const path = require('path');
+// const fs = require('fs/promises');
+// const colors = require("colors");
+
+// // Verficar si la ruta es absoluta o relativa
+// console.log(`Es o no absoluta: ${path.isAbsolute(promptValue)}`.white);
+// if (path.isAbsolute(promptValue) === false) {
+//     console.log('Conviertiendo ruta a absoluta'.magenta)
+//     console.log(`Ruta absoluta: ${path.resolve(promptValue)}`.white);
+// }
+// if (path.extname(promptValue) === '.md') {
+//     console.log('Leyendo archivo'.magenta);
+// } else {
+//     console.log('Su archivo no es de tipo Markdown'.red);
+// }
--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const path = require('path');
-const fs = require('fs/promises');
+const fs = require('fs');
 const colors = require("colors");
 
 // Ingresar ruta
-const prompt = require ( 'prompt-sync' ) ( ) ; 
+const prompt = require('prompt-sync')();
 let promptValue = prompt('Ingresa la ruta del archivo: '.magenta);
 console.log(`Ruta ingresada: ${promptValue}`.white);
 
 // Verficar si la ruta es absoluta o relativa
 console.log(`Es o no absoluta: ${path.isAbsolute(promptValue)}`.white);
-console.log(`Ruta absoluta: ${path.resolve(promptValue)}`.white);
-console.log(`Extension: ${path.extname(promptValue)}`.white);
+
+//Si la ruta es relativa
+let absolutePath = path.resolve(promptValue);
+
+if (path.isAbsolute(promptValue) === false) {
+    console.log('Conviertiendo ruta a absoluta'.magenta)
+    console.log(`Ruta absoluta: ${absolutePath}`.white);
+}
+
+const reading = () => {
+    //console.log(absolutePath);
+    const fileContent = fs.readFileSync(absolutePath, 'utf-8', (error, data) => {
+        if (!error) {
+            return data;
+        } else {
+            return console.log(`Error: ${error}`);
+        }
+    })
+    return fileContent;
+}
+
+if (path.extname(promptValue) === '.md') {
+    console.log('Leyendo archivo'.magenta);
+    console.log(reading());
+} else {
+    console.log('Su archivo no es de tipo Markdown'.red);
+}
