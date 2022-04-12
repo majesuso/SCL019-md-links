5d1dde16886a01d885f7ebc79c1698063ff58583
majesuso <majesus.op@gmail.com>
2022-04-12 18:18:30 -0400

    verificar si la ruta existe

--- functions.js
+++ functions.js
@@ -1,15 +1,20 @@
 // const path = require('path');
-// const fs = require('fs/promises');
+// const fs = require('fs');
 // const colors = require("colors");
 
 // // Verficar si la ruta es absoluta o relativa
-// console.log(`Es o no absoluta: ${path.isAbsolute(promptValue)}`.white);
-// if (path.isAbsolute(promptValue) === false) {
-//     console.log('Conviertiendo ruta a absoluta'.magenta)
-//     console.log(`Ruta absoluta: ${path.resolve(promptValue)}`.white);
+// const absoluteOrRelative = () => {
+//     console.log(`Es o no absoluta: ${path.isAbsolute(promptValue)}`.white);
+    
+//     //Si la ruta es relativa
+//     let absolutePath = path.resolve(promptValue);
+
+//     if (path.isAbsolute(promptValue) === false) {
+//         console.log('Conviertiendo ruta a absoluta'.magenta)
+//         console.log(`Ruta absoluta: ${absolutePath}`.white);
+//     }
 // }
-// if (path.extname(promptValue) === '.md') {
-//     console.log('Leyendo archivo'.magenta);
-// } else {
-//     console.log('Su archivo no es de tipo Markdown'.red);
-// }
+
+// module.exports = {
+//     absoluteOrRelative,
+//   };
--- index.js
+++ index.js
@@ -2,25 +2,39 @@ const path = require('path');
 const fs = require('fs');
 const colors = require("colors");
 
-// Ingresar ruta
+// Entrada para ingresar la ruta
 const prompt = require('prompt-sync')();
-let promptValue = prompt('Ingresa la ruta del archivo: '.magenta);
-console.log(`Ruta ingresada: ${promptValue}`.white);
+let pathValue = prompt('Ingresa la ruta del archivo: '.magenta);
+console.log(`Ruta ingresada: '${pathValue}'`.white);
 
-// Verficar si la ruta es absoluta o relativa
-console.log(`Es o no absoluta: ${path.isAbsolute(promptValue)}`.white);
+// 1. Función que me retorne la ruta absoluta
+const pathAbsoluteOrRelative = (route) => {
+    // 1.1 Verficar si la ruta es absoluta o relativa
+    let questionIsAbsolute = path.isAbsolute(route);
+    //console.log(`Es o no absoluta: ${questionIsAbsolute}`.white);
 
-//Si la ruta es relativa
-let absolutePath = path.resolve(promptValue);
-
-if (path.isAbsolute(promptValue) === false) {
-    console.log('Conviertiendo ruta a absoluta'.magenta)
-    console.log(`Ruta absoluta: ${absolutePath}`.white);
+    // 1.2 Condicional si es que es absoluta la retorna tal cual, si es que es relativa la transforma
+    const absolutePath = () => {
+        if (questionIsAbsolute === false) {
+            let absolutePathValue = path.resolve(route);
+            //console.log('Conviertiendo ruta a absoluta'.magenta);
+            //console.log(`Ruta absoluta: ${absolutePathValue}`.white);
+            return absolutePathValue;
+        } else {
+            return route;
+        }
+    }
+    return absolutePath();
 }
 
+let pathTransformed = pathAbsoluteOrRelative(pathValue);
+pathTransformed = path.normalize(pathTransformed); // Normaliza la ruta
+const pathExists = (route) => fs.existsSync(route); // Función para corroborar si la ruta existe
+
+// Función para acceder al contenido del archivo
 const reading = () => {
     //console.log(absolutePath);
-    const fileContent = fs.readFileSync(absolutePath, 'utf-8', (error, data) => {
+    const fileContent = fs.readFileSync(pathTransformed, 'utf-8', (error, data) => {
         if (!error) {
             return data;
         } else {
@@ -30,9 +44,16 @@ const reading = () => {
     return fileContent;
 }
 
-if (path.extname(promptValue) === '.md') {
+// Acceder al archivo SI su extensión es '.md' y 
+if ((path.extname(pathTransformed) === '.md') && (pathExists(pathTransformed) === true)) {
     console.log('Leyendo archivo'.magenta);
-    console.log(reading());
-} else {
-    console.log('Su archivo no es de tipo Markdown'.red);
+    //console.log(reading());
+}
+
+else if ((path.extname(pathTransformed) !== '.md') && (pathExists(pathTransformed) === true)) {
+    return console.log('Su archivo no es de tipo Markdown'.red);
+}
+
+else if (pathExists(pathTransformed) === false) {
+    return console.log('La ruta no existe, verifique la ruta y vuelva a ingresar'.red)
 }
